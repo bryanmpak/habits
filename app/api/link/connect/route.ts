e012fc8429ab823ae15da72883fc25a029447238
Bryan Pak <bryan.m.pak@gmail.com>
2024-03-29 16:31:11 +0900

    comment out old code

--- app/api/link/connect/route.ts
+++ app/api/link/connect/route.ts
@@ -1,69 +1,69 @@
-import { getAuthSession } from "@/lib/auth"
-import { prisma } from "@/lib/prisma"
-import { NextRequest } from "next/server"
+// import { getAuthSession } from "@/lib/auth"
+// import { prisma } from "@/lib/prisma"
+// import { NextRequest } from "next/server"
 
-export async function GET(req: NextRequest) {
-  const session = await getAuthSession()
-  if (!session) {
-    return new Response("Unauthorized", { status: 403 })
-  }
-  const userEmail = session?.user.email as string
+// export async function GET(req: NextRequest) {
+//   const session = await getAuthSession()
+//   if (!session) {
+//     return new Response("Unauthorized", { status: 403 })
+//   }
+//   const userEmail = session?.user.email as string
 
-  try {
-    const data = await prisma.linkingRequest.findFirst({
-      where: { responderEmail: userEmail },
-      select: { secretQuestion: true },
-    })
+//   try {
+//     const data = await prisma.linkingRequest.findFirst({
+//       where: { responderEmail: userEmail },
+//       select: { secretQuestion: true },
+//     })
 
-    return new Response(JSON.stringify(data), { status: 200 })
-  } catch (error) {
-    return new Response(null, { status: 500 })
-  }
-}
+//     return new Response(JSON.stringify(data), { status: 200 })
+//   } catch (error) {
+//     return new Response(null, { status: 500 })
+//   }
+// }
 
-export async function POST(req: NextRequest) {
-  const session = await getAuthSession()
-  const userId = session?.user.id as string
-  const userEmail = session?.user.email as string
+// export async function POST(req: NextRequest) {
+//   const session = await getAuthSession()
+//   const userId = session?.user.id as string
+//   const userEmail = session?.user.email as string
 
-  if (!session) {
-    return new Response("Unauthorized", { status: 403 })
-  }
+//   if (!session) {
+//     return new Response("Unauthorized", { status: 403 })
+//   }
 
-  try {
-    const body = await req.json()
-    const { answer } = body
+//   try {
+//     const body = await req.json()
+//     const { answer } = body
 
-    // *** for now, need to figure out a hash solution
-    const hashedAnswer = answer
+//     // *** for now, need to figure out a hash solution
+//     const hashedAnswer = answer
 
-    const request = await prisma.linkingRequest.findFirst({
-      where: { responderEmail: userEmail },
-    })
+//     const request = await prisma.linkingRequest.findFirst({
+//       where: { responderEmail: userEmail },
+//     })
 
-    if (!request || request.responderEmail !== session?.user.email) {
-      return new Response(
-        "Request not found or you're not the intended recipient",
-        { status: 404 }
-      )
-    }
+//     if (!request || request.responderEmail !== session?.user.email) {
+//       return new Response(
+//         "Request not found or you're not the intended recipient",
+//         { status: 404 }
+//       )
+//     }
 
-    if (hashedAnswer !== request.secretAnswerHash) {
-      return new Response("Incorrect Answer", { status: 403 })
-    }
+//     if (hashedAnswer !== request.secretAnswerHash) {
+//       return new Response("Incorrect Answer", { status: 403 })
+//     }
 
-    await prisma.user.update({
-      where: { id: userId },
-      data: { linkedUserId: request.requestorId },
-    })
+//     await prisma.user.update({
+//       where: { id: userId },
+//       data: { linkedUserId: request.requestorId },
+//     })
 
-    await prisma.user.update({
-      where: { id: request.requestorId },
-      data: { linkedUserId: userId },
-    })
+//     await prisma.user.update({
+//       where: { id: request.requestorId },
+//       data: { linkedUserId: userId },
+//     })
 
-    return new Response("Users linked successfully", { status: 200 })
-  } catch (error) {
-    return new Response("Something went wrong", { status: 400 })
-  }
-}
+//     return new Response("Users linked successfully", { status: 200 })
+//   } catch (error) {
+//     return new Response("Something went wrong", { status: 400 })
+//   }
+// }
--- components/LinkConnectForm.tsx
+++ components/LinkConnectForm.tsx
@@ -1,64 +1,64 @@
-"use client"
+// "use client"
 
-import { useRouter } from "next/navigation"
-import React, { useState } from "react"
-import { toast } from "sonner"
+// import { useRouter } from "next/navigation"
+// import React, { useState } from "react"
+// import { toast } from "sonner"
 
-const LinkConnectForm = () => {
-  const [answer, setAnswer] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const router = useRouter()
+// const LinkConnectForm = () => {
+//   const [answer, setAnswer] = useState("")
+//   const [isLoading, setIsLoading] = useState(false)
+//   const router = useRouter()
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setIsLoading(true)
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault()
+//     setIsLoading(true)
 
-    try {
-      const response = await fetch("/api/link/connect", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ answer }),
-      })
-      if (response.ok) {
-        toast("correct answer", {
-          description: "enjoy using your shared habits app 🥳",
-        })
-        router.push("/")
-        router.refresh()
-      }
-    } catch (error) {
-      console.log(error)
-      toast.error("something went wrong", {
-        description: "please try again",
-      })
-    } finally {
-      setIsLoading(false)
-    }
-  }
+//     try {
+//       const response = await fetch("/api/link/connect", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ answer }),
+//       })
+//       if (response.ok) {
+//         toast("correct answer", {
+//           description: "enjoy using your shared habits app 🥳",
+//         })
+//         router.push("/")
+//         router.refresh()
+//       }
+//     } catch (error) {
+//       console.log(error)
+//       toast.error("something went wrong", {
+//         description: "please try again",
+//       })
+//     } finally {
+//       setIsLoading(false)
+//     }
+//   }
 
-  return (
-    <form
-      className='flex flex-col gap-2 w-full'
-      onSubmit={(e) => handleSubmit(e)}
-    >
-      <input
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-        required
-        placeholder='enter secret answer'
-        className='p-2 bg-neutral border border-neutral text-title focus:border-shadow border-1 focus:outline-none'
-      ></input>
-      <button
-        aria-label='Submit secret answer'
-        className='flex mt-8 items-center justify-center p-2 w-full h-12 text-black bg-title hover:bg-text'
-        type='submit'
-      >
-        {isLoading ? "checking answer.." : "submit answer"}
-      </button>
-    </form>
-  )
-}
+//   return (
+//     <form
+//       className='flex flex-col gap-2 w-full'
+//       onSubmit={(e) => handleSubmit(e)}
+//     >
+//       <input
+//         value={answer}
+//         onChange={(e) => setAnswer(e.target.value)}
+//         required
+//         placeholder='enter secret answer'
+//         className='p-2 bg-neutral border border-neutral text-title focus:border-shadow border-1 focus:outline-none'
+//       ></input>
+//       <button
+//         aria-label='Submit secret answer'
+//         className='flex mt-8 items-center justify-center p-2 w-full h-12 text-black bg-title hover:bg-text'
+//         type='submit'
+//       >
+//         {isLoading ? "checking answer.." : "submit answer"}
+//       </button>
+//     </form>
+//   )
+// }
 
-export default LinkConnectForm
+// export default LinkConnectForm
